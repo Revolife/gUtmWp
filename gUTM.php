ac1bcaa5f8ac4a2d89d412288a095db5386fa9c3
Grom <grom@revolife.com>
2015-06-30 14:06:35 +0300

    init

---
+++ gUTM.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package gUtmWp
+ * @author Grom <grom@revolife.com>
+ * @version 1.0
+ */
+
+/*
+Plugin Name: Google UTM WP
+Plugin URI: https://github.com/Revolife/gUtmWp
+Description: determine the Google UTM (Urchin Tracking Module) tails.
+Version: 1.0
+Author: Grom
+Author URI: https://revolife.com
+License: GPL2
+*/
+
