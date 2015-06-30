38b03904164cf07b22602edb8f32fce674e2928c
Grom <grom@revolife.com>
2015-06-30 15:23:41 +0300

    v1.0

---
+++ core/gutmwp.php
@@ -0,0 +1,32 @@
+<?php
+class gUtmWp{
+	public static function start(){
+		self::bindWpActions();
+		self::bindWpFilters();
+	}
+
+	private static function bindWpActions(){
+		add_action('wp_head',[ 'gUTM', 'init']);
+	}
+
+	private static function bindWpFilters(){
+		add_filter( 'wp_mail', [ __CLASS__, 'wpmailFilter']);
+
+	}
+
+	public static function wpmailFilter($args ){
+		$args['message'] = self::replaceUtmData($args['message']);
+		return $args;
+	}
+
+	private static function replaceUtmData($message){
+		$utm = gUTM::getLastUtmConversion();
+		$replace= '';
+		if(!empty($utm))
+			foreach ( $utm as $k=>$v ) {
+				$replace .= "$k : $v".PHP_EOL;
+			}
+
+		return str_replace('[gUTM]',$replace,$message);
+	}
+}
--- gUtmWp.php
+++ gUtmWp.php
@@ -15,3 +15,10 @@ Author URI: https://revolife.com
 License: GPL2
 */
 
+defined('DS') or define('DS',DIRECTORY_SEPARATOR);
+defined('gUtmpWp_PLUG_PATH') or define('gUtmpWp_PLUG_PATH',dirname(__FILE__));
+
+require_once(gUtmpWp_PLUG_PATH.DS.'core'.DS.'classes'.DS.'gutm.php');
+require_once(gUtmpWp_PLUG_PATH.DS.'core'.DS.'gutmwp.php');
+
+gUtmWp::start();
